cd493f0a928ee4abc2206d69e0287961b3f1f42b
Tim <timothy.mace@outlook.com>
2018-11-05 16:47:02 -0500

    work plz

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-18-222-188-203.us-east-2.compute.amazonaws.com',
+      host: 'ec2-18-224-229-225.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/tutorial.pem',
       ref: 'origin/master',
       repo: 'git@github.com:FrTime/aws-tutorial.git',
