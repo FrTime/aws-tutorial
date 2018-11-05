b19dbd87318a55dc6cdfeaa267b1d1be7cebd435
Tim <timothy.mace@outlook.com>
2018-11-05 16:13:08 -0500

    deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'aws-tutorial',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-222-188-203.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/tutorial.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:FrTime/aws-tutorial.git',
+      path: '/home/ubuntu/server',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
